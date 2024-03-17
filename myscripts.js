6e1ad7ec8ae22e5d5f867b4d69edf459bf670a9a
mkreynolds919 <mkreynolds919@gmail.com>
2024-03-16 21:02:45 -0500

    Add playerChoice function and validation

--- myscripts.js
+++ myscripts.js
@@ -1,7 +1,30 @@
+
+const choices = ["ROCK", "PAPER", "SCISSORS"];
+
 function getComputerChoice() {
-    let choices = ['Rock', 'Paper', 'Scissors'];
-    let computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    let computerChoice = choices[Math.floor(Math.random() * choices.length)].toUpperCase();
     return computerChoice;
 }
 
-console.log(getComputerChoice);
+function choiceValid(choice) {
+    if (choice in choices) {
+        return true;
+    } else {
+        console.log("Selection must be Rock, Paper, or Scissors");
+    }
+}
+
+
+function getPlayerChoice() {
+    let playerChoice = prompt("What would you like to choose?").trim().toUpperCase(); 
+    if (choices.includes(playerChoice)) {
+        return playerChoice;
+    } else {
+        console.log("Selection must be Rock, Paper, or Scissors");
+    }
+}
+
+console.log(getPlayerChoice(), getComputerChoice());
+
+
+
